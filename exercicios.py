fdc3e73981d6ec410870c3c1c5192ace700a83a2
gbentosilva <gabrielbento9812@gmail.com>
2024-11-30 07:44:18 -0300

    exercícios

---
+++ exercicios.py
@@ -0,0 +1,9 @@
+#exercicio 1
+
+quantidade = 40
+preço = -2
+
+if preço > 0 and quantidade > 0:
+    print("O preço é positivo e a quantidade é maior que zero.")
+else:
+    print("O preço é negativo ou a quantidade é zero ou negativa.")
