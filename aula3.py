173797f51ef74b9303a94a902c1d470df3084eee
gbentosilva <gabrielbento9812@gmail.com>
2024-11-30 08:22:13 -0300

    Aula 3 e exercícios

---
+++ aula3.py
@@ -0,0 +1,8 @@
+x = 2
+
+if x <= 0:
+    print('menor que zero')
+elif x <2 :
+    print('menor que 2')
+else:
+    print('maior ou igual a 2')
---
+++ for.py
@@ -0,0 +1,9 @@
+for i in range(1,5):
+    print(i)
+
+lista_de_alunos = ["rafael", "fabio", "luciano"]
+
+for aluno in lista_de_alunos:
+    print(aluno)
+
+    
